7b4dc62f37019d896fde4c4994628822ec74bc98
LGXTvT <1336037686@qq.com>
2022-05-13 21:52:18 +0800

    添加系统监控-数据库监控

---
+++ src/views/jy-monitor/database/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <iframe :src="iframeUrl" width="100%" height="100%" frameborder="0"  id="docIframe"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataBase",
+  data() {
+    return {
+      iframeUrl: 'http://localhost:8080/druid'
+    }
+  },
+  mounted(){
+    /**
+     * iframe-宽高自适应显示
+     */
+    const oIframe = document.getElementById('docIframe');
+    const deviceWidth = document.documentElement.clientWidth;
+    const deviceHeight = document.documentElement.clientHeight;
+    oIframe.style.width = (deviceWidth - 50) + 'px';
+    oIframe.style.height = (deviceHeight - 100) + 'px';
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
